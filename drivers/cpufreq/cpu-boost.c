e0c0d2dea426c2501cfa6dc9ca68a8cf52cd786e
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-25 19:17:55 +0000

    cpufreq: cpu-boost: don't boost over user set max freq
    It was negating the user set max cpu freq and boosting over it if for example input_boost_freq > max_freq, so fix it
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -142,9 +142,7 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 
 		ib_min = min(ib_min, policy->max);
 
-		pr_debug("CPU%u policy min before boost: %u kHz\n",
-			 cpu, policy->min);
-		pr_debug("CPU%u boost min: %u kHz\n", cpu, ib_min);
+	min = min(min, policy->max);
 
 		cpufreq_verify_within_limits(policy, ib_min, UINT_MAX);
 
